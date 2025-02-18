d2abb39bc28dc93f01f1fe279a370438d2355325
dohyunkim-furiosa <155939201+dohyunkim-furiosa@users.noreply.github.com>
2025-02-18 15:47:32 +0900

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -22,23 +22,24 @@ cargo fmt --all -- --check
 cargo clippy --all-targets -- -D warnings
 cargo sort --grouped --check --workspace
 ruff check scripts
-ruff format --check --diff scripts
-$HOME/scripts/pre-push.sh" > .git/hooks/pre-push
+ruff format --check --diff scripts" > .git/hooks/pre-push
 chmod u+x .git/hooks/pre-push
-
 
 rustup default nightly
 ##### furiosa/npu-tools README.md #####
+rustup target add aarch64-unknown-none-softfloat
+
+apt install -y build-essential clang libncurses-dev libssl-dev pkg-config python3-dev gcc-aarch64-linux-gnu libboost-dev libboost-regex-dev libelf-dev cmake libtbb-dev clang-format-11 clang-tidy libc6-dev-arm64-cross libyaml-cpp-dev libgl1-mesa-glx libcapstone-dev ninja-build python-is-python3
+pip3 install -U pip wheel
+pip3 install -r tekton/build/requirements.txt
+pip3 install fbgemm-gpu-cpu
+
 PROTOC_VERSION=22.0
 curl -Lo protoc.zip "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip"  \
 && unzip -q protoc.zip bin/protoc -d /usr/local \
 && chmod a+x /usr/local/bin/protoc
 PATH="/usr/local/bin:$PATH"
-rustup target add aarch64-unknown-none-softfloat
-apt install -y build-essential clang libncurses-dev libssl-dev pkg-config python3-dev gcc-aarch64-linux-gnu libboost-dev libboost-regex-dev libelf-dev cmake libtbb-dev clang-format-11 clang-tidy libc6-dev-arm64-cross libyaml-cpp-dev libgl1-mesa-glx libcapstone-dev ninja-build clang libc++-dev libc++abi-dev
-pip3 install -U pip wheel
-pip3 install -r tekton/build/requirements.txt
-pip3 install fbgemm-gpu-cpu
+rm protoc.zip
 ############################################
 pip3 install dvc[s3]
 
@@ -55,13 +56,10 @@ ln -s /cache/llm_dfg_cache/ crates/npu-torch-models/llm_dfg_cache/.dvc/cache
 
 cargo install cargo-sort --locked
 cargo install cargo-nextest --locked
-cargo use_renegade --release
-
 
 mkdir -p $HOME/.aws
 echo "
 TODO:
-  1. Update aws credentials from https://aws-cli.furiosa.dev
-  2. dvc --cd $NAME/artifacts/furiosa-libtorch/jammy pull -r origin -j 10
-  3. dvc --cd $NAME pull -r origin -j 10
+  1. Follow the instructions in https://github.com/furiosa-ai/npu-tools/blob/master/README.md
+  2. cargo use_renegade -r && cargo build
 "
